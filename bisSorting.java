b604fbf0493e55bb1b8430a1bc80ab6356cdc87e
Mukul_kYadav <ymukul6180@gmail.com>
2022-10-07 19:53:22 +0530

    all three sorting methods completed

--- bisSorting.java
+++ bisSorting.java
@@ -40,7 +40,19 @@ public class bisSorting {
     }
 
     public static void insertionsort(int arr[]) {
-        
+        for (int i = 1; i < arr.length; i++) {
+            int curr = arr[i];
+            int j = i-1;
+            while (j>=0 && arr[j]>curr) {
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=curr;
+        }
+        System.out.print("Insertion sorted arra is: ");
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
     }
 
     public static void selectionsort(int arr[]) {
